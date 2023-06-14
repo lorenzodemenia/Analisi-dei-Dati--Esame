16a71e7fd2e1aefaccaccbe33217a9cdaaaa1946
Lorenzo Demenia <71456537+lorenzodemenia@users.noreply.github.com>
2023-06-14 21:29:30 +0200

    es 5

--- ESAME/Unità 4/ESERCIZI/Esercizi_test.R
+++ ESAME/Unità 4/ESERCIZI/Esercizi_test.R
@@ -1,3 +1,10 @@
-A <- c(9, 9, 6, 10, 3, 9, 4, 5, 3, 7)
-t.test(A, conf.level = .99)
+z <- qnorm(0.975)
+0.25 + c(-1, 1)* z * sqrt((0.25*(1-0.25))/55)
 
+tab_oss <- as.table(matrix(c(18, 16, 16, 25, 11, 14), nrow = 2))
+names(dimnames(tab_oss)) <- c('Europa', 'Preferenza')
+# display
+plot(tab_oss, col = c("firebrick", "darkolivegreen"), 
+main = "Preferenze Europa", xlab = 'Europa', ylab = 'Preferenza' )
+
+summary(tab_oss)
